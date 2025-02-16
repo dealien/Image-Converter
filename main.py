31e4a528a97bdc16cea0112ea1b6960a02f7d030
Vyren <dragonofawsome@gmail.com>
2025-02-15 23:01:16 -0500

    Basic args and background removal

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
+import argparse
 from file_management import *
+from remove_background import *
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description="Process command line arguments.")
+parser.add_argument('file', type=str, nargs='?', default=None, help='the image file path')
+parser.add_argument('-bg', '--remove-background', action='store_true', help='remove image background')
+args = parser.parse_args()
+
+# TODO: If no arguments are passed, switch to a menu
+# TODO: File path accepts wildcards
 
 # Move images to the Base Images directory
 move_images_to_subdirectory('Base Images')
 
+# Parse arguments
+print(args)
+print(f"File: {args.file}")
+print(f"Remove background: {args.remove_background}")
+
+if not args.file:
+    print('No file specified. Exiting...')
+    exit()
+try:
+    # TODO: If '*' passed, execute for every file
+    input_image = Image.open(args.file)
+except Exception as e:
+    print(f'Error while loading file: {e}')
+    exit()
+
+# Execute selected commands
+if args.remove_background:
+    output_image = remove_background(input_image)
+else:
+    print('No actions specified. Exiting...')
+    exit()
+
+# Saves final output image
+if not os.path.exists('Output/'):
+    os.makedirs('Output/')
+output_image.save("Output/.tmp.png")
+filename = Path(args.file).stem
+os.replace("Output/.tmp.png", 'Output/' + filename + '.png')
+print("Image saved successfully:", 'Output/' + filename + '.png')
---
+++ remove_background.py
@@ -0,0 +1,30 @@
+import os
+from rembg import remove
+from PIL import Image, ImageOps, ImageChops, ImageFile
+
+
+def remove_background(image_input: ImageFile, opt_border_width: int = 0):
+    """
+    Remove the background from an image.
+
+    :param image_input: The image to modify.
+    :param opt_border_width: The number of pixels to be removed from the border.
+    :return:
+    """
+
+    # Add white border
+    image_input = ImageOps.expand(image_input, border=int(opt_border_width))
+    # Removes background
+    output = remove(image_input)
+    # Removes white border that .expand() added
+    output = trim(output)
+    return output
+
+
+def trim(image):
+    bg = Image.new(image.mode, image.size, image.getpixel((0, 0)))
+    diff = ImageChops.difference(image, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return image.crop(bbox)
