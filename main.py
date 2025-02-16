729deba18496af85663852606c256de7dd9b9361
Vyren <dragonofawsome@gmail.com>
2025-02-15 23:33:52 -0500

    Basic iteration over all images in directory

--- main.py
+++ main.py
@@ -15,31 +15,38 @@ args = parser.parse_args()
 move_images_to_subdirectory('Base Images')
 
 # Parse arguments
-print(args)
-print(f"File: {args.file}")
-print(f"Remove background: {args.remove_background}")
+# print(args)
+# print(f"File: {args.file}")
+# print(f"Remove background: {args.remove_background}")
 
-if not args.file:
-    print('No file specified. Exiting...')
-    exit()
-try:
-    # TODO: If '*' passed, execute for every file
-    input_image = Image.open(args.file)
-except Exception as e:
-    print(f'Error while loading file: {e}')
-    exit()
+if not args.file or args.file == '*':  # For all files in "Base Images" directory
+    images = []
+    for root, dirs, files in os.walk('Base Images/'):
+        for file in files:
+            input_image = Image.open('Base Images/' + file)
+            images.append([file, input_image])
+            # print([file, input_image])
+else:  # For single specified file
+    try:
+        input_image = Image.open(args.file)
+        images = [[args.file, input_image]]
+    except Exception as e:
+        print(f'Error while loading file: {e}')
+        exit()
 
-# Execute selected commands
-if args.remove_background:
-    output_image = remove_background(input_image)
-else:
-    print('No actions specified. Exiting...')
-    exit()
+for image in images:
+    # print(image)
+    # Execute selected commands
+    if args.remove_background:
+        output_image = remove_background(image[1])
+    else:
+        print('No actions specified. Exiting...')
+        exit()
 
-# Saves final output image
-if not os.path.exists('Output/'):
-    os.makedirs('Output/')
-output_image.save("Output/.tmp.png")
-filename = Path(args.file).stem
-os.replace("Output/.tmp.png", 'Output/' + filename + '.png')
-print("Image saved successfully:", 'Output/' + filename + '.png')
+    # Saves final output image
+    if not os.path.exists('Output/'):
+        os.makedirs('Output/')
+    output_image.save("Output/.tmp.png")
+    filename = Path(image[0]).stem
+    os.replace("Output/.tmp.png", 'Output/' + filename + '.png')
+    print("Image saved successfully:", 'Output/' + filename + '.png')
